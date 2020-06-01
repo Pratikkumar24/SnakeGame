3cfe35a9bee630839cdc2b0dd94e0459b39487f6
Pratik24 <pratikkumar200024@gmail.com>
2020-06-01 13:40:28 +0530

    starting of the game

---
+++ start.cpp
@@ -0,0 +1,50 @@
+#include <unistd.h>
+#include "terminal_io.h"
+#include "classSnake.h"
+#include <thread>
+#include <time.h>
+void Snake::input()
+{
+    if (c = getch())
+    {
+        switch (c)
+        {
+        case 'w':
+            c = 'w';
+            break;
+        case 'a':
+            c = 'a';
+            break;
+        case 'd':
+            c = 'd';
+            break;
+        case 's':
+            c = 's';
+            break;
+        default:
+            c = c;
+        }
+    }
+}
+void Snake::StartGame()
+{
+    system("clear");
+
+    display();
+    cout << "\n value of posx:" << posx << " and posy:" << posy;
+    c = getch();
+    while (1)
+    {
+        system("clear");
+        moving(c);
+        cout << "\n value of posx:" << posx << " and posy:" << posy;
+
+        display();
+        usleep(199000);
+        // sleep(1); //Sleep for 1 second
+        thread t1([this] {
+            this->input();
+        });
+        t1.detach();
+    }
+}
